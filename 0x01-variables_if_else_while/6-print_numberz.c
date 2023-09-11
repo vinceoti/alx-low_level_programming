0515c255cb35d30147df4d58d40d0d75bca16caf
vinceoti <vincentoo32@outlook.com>
2023-09-11 12:57:39 -0700

    updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+	
+		putchar(i);
+	
+	putchar('\n');
+	return (0);
+}
+
