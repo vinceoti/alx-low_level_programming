a925bc1cee98a5d753bc56cb50cb173321cf5136
vinceoti <vincentoo32@outlook.com>
2023-09-11 12:54:04 -0700

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,19 +3,19 @@
 #include <string.h>
 
 /**
- * main - Prints the alphabet without q and e.
+ * main - Outputs the alphabet except q and e.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 97; i < 123; i++)
+	for (x = 97; x < 123; x++)
 	{
-		if (i != 101 && i != 113)
+		if (x != 101 && x != 113)
 		{
-			putchar(i);
+			putchar(x);
 		}
 	}
 	putchar('\n');
