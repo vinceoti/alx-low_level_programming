0e84f2cc08213090321e3f8693a6679fb1f0dfeb
vinceoti <vincentoo32@outlook.com>
2023-09-11 11:26:03 -0700

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there 
+/**main returns small alphabets
 *Alpabets in small letters 
-* betty style doc for function main goes there
+* betty style doc for function main goes there 
 */
-
 int main(void)
 {
 	char a[26] = "abcdefghijklmnopqrstuvwxyz";
-	
 	int x;
-	for (x = 0 ; x < 26; x++)
+	for (x = 0; x < 26; x++)
 		putchar(a[x]);
 	putchar('\n');
 	return (0);
