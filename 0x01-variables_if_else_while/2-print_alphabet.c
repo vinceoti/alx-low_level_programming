d2f67ce1b2a21d290f3347be6595312a53c4d638
vinceoti <vincentoo32@outlook.com>
2023-09-11 11:19:06 -0700

    updated

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/*Alpabets in small letters*/ 
-/* betty style doc for function main goes there */
+/* more headers goes there 
+*Alpabets in small letters 
+* betty style doc for function main goes there
+*/
+
 int main(void)
 {
 	char a[26] = "abcdefghijklmnopqrstuvwxyz";
