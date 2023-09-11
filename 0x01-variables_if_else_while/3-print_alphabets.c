2875dc16bb349f019889622dbd5976aa5997b441
vinceoti <vincentoo32@outlook.com>
2023-09-11 11:57:07 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ";
+	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int x;
 
 	for (x = 0; x < 52; x++)
