e6a77578bfad989666baccc7471ccbe1914813fe
vinceoti <vincentoo32@outlook.com>
2023-09-11 12:22:34 -0700

    updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Outputs the alphabet excluding q and e.
+ *
+ * Return: Always 0 for Success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+	
+		if (x != 101 && x != 113)
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - writes numbers 0  upto 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	for (x < 10; x++)
+	
+		printf("%d", x);
+	
+	putchar('\n');
+	return (0);
+}
+
