57c79e8171139b2a793ea9f9004b17c780618a11
vinceoti <vincentoo32@outlook.com>
2023-09-11 18:05:31 -0700

    updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - gives combinations of single-digit numbers
+ * Return: Mostly 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; n++)
+	
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	
+	putchar('\n');
+	return (0);
+}
